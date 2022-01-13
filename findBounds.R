370f3d03feca786d37515f6e2c1ac004fa4ea716
Ethan Jantz <21095848+EthanJantz@users.noreply.github.com>
2022-01-13 15:11:35 -0600

    Initial commit

---
+++ findBounds.R
@@ -0,0 +1,48 @@
+findBounds <- function(t, s) {
+  if (!(st_is(t, "POLYGON"))) stop("t must be a POLYGON")
+  if (!any(st_is(s, "LINESTRING"))) stop("s must contain only LINESTRING geometries")
+  
+  tract_streets <- s %>%
+    st_filter(t)
+  
+  tract_line <- st_cast(t, "MULTILINESTRING")
+  tract_buffer <- st_buffer(tract_line, dist = 5)
+  
+  streets_buffer <- tract_streets %>% 
+    group_by(streetname) %>%
+    summarise() %>%
+    st_cast("LINESTRING") %>%
+    st_buffer(dist = 1) %>% 
+    mutate(area = st_area(geometry))
+  
+  intersection <- st_intersection(streets_buffer, tract_buffer) %>%
+    st_make_valid()
+  
+  bounding_streets <- intersection %>%
+    # Find how much of the intersected area remains of the original geometry area
+    mutate(i_area = as.numeric(st_area(geometry)),
+           pct_area = as.numeric(i_area/area)) %>%
+    filter(pct_area > .3) %>%
+    pull(streetname)
+  
+  street_bounds <- tract_streets %>%
+    filter(streetname %in% bounding_streets) %>%
+    distinct(streetname, .keep_all = T) %>%
+    group_by(ewns_split = ifelse(ewns_dir %in% c("S", "N"),  "North-South", "East-West")) %>%
+    mutate(
+      side = case_when(
+        ewns_split == "East-West" & ewns == max(ewns) ~ "East",
+        ewns_split == "East-West" & ewns == min(ewns) ~ "West",
+        ewns_split == "North-South" & ewns == max(ewns) ~ "North",
+        ewns_split == "North-South" & ewns == min(ewns) ~ "South",
+        max(ewns) == min(ewns) ~ "No Opposite Bound",
+        TRUE ~ "Additional Side"
+      ),
+      ewns_text = paste0(side, " - ", street_nam)
+    ) %>%
+    pull(ewns_text)
+  
+  street_bounds <- list(street_bounds)
+
+  return(street_bounds)
+}
---
+++ main.R
@@ -0,0 +1,19 @@
+library(dplyr)
+library(sf)
+
+tracts <- read_sf(here::here("Data", "tracts_2010", "tracts.shp"))
+streets <- read_sf(here::here("Data", "streets", "streets_chi.shp"))
+
+source("findBounds.R")
+
+# This will take a significant amount of time
+for(tract in 1:nrow(tracts)) {
+  print(paste0("Row ", tract, " of ", nrow(tracts)))
+  t <- tracts[tract, ]
+  bounds <- findBounds(t, streets)
+  tracts$bounds[tract] <- bounds
+}
+
+# Convert list col to comma-separated string and save
+unnest(tracts, bounds) %>%
+  st_write(here::here("Output", "tracts_bounds.shp"))
